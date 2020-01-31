7cca26b911cf5423cc280dfca3f8fa231c3cfa23
volzock <nukfeed@yandex.ru>
2020-01-31 16:31:02 +0300

    New ayes

--- pc_vis/digits.py
+++ pc_vis/digits.py
@@ -1,24 +1,33 @@
+from PIL import Image
 import pytesseract as pytes
-from PIL import Image 
 import cv2
+import numpy as np
 
-def print_digits_from_img():
-    cap = cv2.VideoCapture(0)
+def main():
+    image = input("Write name of img file: ")
+    img = cv2.imread(image)
+    print("[System] Image had read")
 
-    while True:
-        ret, frame = cap.read()
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        hsv = cv2.inRange(hsv, (0, 0, 159), (75, 33, 255))
-        contours = cv2.findContours(hsv, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-        contours = contours[0]
-        if contours:
-            contours = sorted(contours, key=cv2.contourArea, reverse=True)
-            (x, y, w, h) = cv2.boundingRect(contours[0])
-        roimg = hsv[y : y+h, x : x+w]
-        cv2.imshow("hsv", hsv)
-        cv2.imshow("frame", roimg)
-        print(pytes.image_to_string(Image.fromarray(hsv), lang="eng"))
-        if cv2.waitKey(1) == 27:
-            break
+    img = cv2.bitwise_not(img)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    hsv_min = np.array((0, 0, 110), np.uint8)
+    hsv_max = np.array((169, 255, 255), np.uint8)
+    img_erode = cv2.inRange(img, hsv_min, hsv_max)
+    img_erode = cv2.erode(img_erode, np.ones((3, 3), np.uint8), iterations=1)
 
-print_digits_from_img()
+    contours, hierarchy = cv2.findContours(img_erode, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+    output = img.copy()
+
+    for idx, contour in enumerate(contours):
+        (x, y, w, h) = cv2.boundingRect(contour)
+        if hierarchy[0][idx][3] == 0:
+            cv2.rectangle(output, (x, y), (x+w, y+h), (70, 0, 0), 1)
+            print("[System] app found ", pytes.image_to_string(Image.fromarray(img[y : y+h, x : x+w]), lang="eng", config='--psm 10 --oem 3 -c tessedit_char_whitelist=0123456789'))
+    print(pytes.image_to_string(Image.fromarray(img_erode), lang="eng", config='--psm 10 --oem 3 -c tessedit_char_whitelist=0123456789'))
+    cv2.imshow("Input", img)
+    cv2.imshow("Enlarged", img_erode)
+    cv2.imshow("Output", output)
+    cv2.waitKey(0)
+
+main()
